281e0355966fa99e33ab063c11e862efc91c7092
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-06-20 01:05:54 -0300

    se arreglo cosas del subHeader

--- src/ToDo/List/SubHeader/SubHeader.js
+++ src/ToDo/List/SubHeader/SubHeader.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
+import { Link } from 'react-router-dom';
 
 const SubHeader = () => {
     return (
         <Box border='1px solid #D9D9D9'>
             <AppBar position="static" color='header'>
                 <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1}}>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
                         My Projects
                     </Typography>
-                    <Button variant='contained' size='small' color='secondary'>
-                        <AddIcon />
-                        Add project
-                    </Button>
+                    <Link to='/add'>
+                        <Button variant='contained' size='small' color='secondary' sx={{ padding: '10px' }} startIcon={<AddIcon sx={{fontSize:'23px !important'}} />}>
+                            Add project
+                        </Button>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </Box>
