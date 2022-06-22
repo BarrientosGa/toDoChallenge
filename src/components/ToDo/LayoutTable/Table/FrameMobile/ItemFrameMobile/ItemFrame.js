2ad8e78224243e5380abd25015fcdb3d1f8639b7
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-06-21 23:08:41 -0300

    se agrego divider

--- src/components/ToDo/LayoutTable/Table/FrameMobile/ItemFrameMobile/ItemFrame.js
+++ src/components/ToDo/LayoutTable/Table/FrameMobile/ItemFrameMobile/ItemFrame.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar,Container, Grid, Typography } from '@mui/material'
+import { Avatar, Container, Divider, Grid, Typography } from '@mui/material'
 import MenuOptions from '../../MenuOptions/MenuOptions';
 import { useStyles } from '../../../styles/frameMobileStyles'
 
@@ -8,27 +8,30 @@ const ItemFrame = ({ project }) => {
     const classes = useStyles()
 
     return (
-        <Container className={classes.container}>
-            <Grid item container xs={12} >
-                <Grid item className={classes.itemColumn}>
-                    <Typography variant='subtitle1'>
-                        {projectName}
-                    </Typography>
-                    <Typography variant='subtitle2'>
-                        Creation date: {date}
+        <>
+            <Container className={classes.container}>
+                <Grid item container xs={12} >
+                    <Grid item className={classes.itemColumn}>
+                        <Typography variant='subtitle1'>
+                            {projectName}
+                        </Typography>
+                        <Typography variant='subtitle2'>
+                            Creation date: {date}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <MenuOptions id={id} />
+                    </Grid>
+                </Grid>
+                <Grid item container xs={12} alignItems='center' sx={{ marginTop: '10px' }}>
+                    <Avatar />
+                    <Typography className={classes.typography}>
+                        {assignedTo}
                     </Typography>
                 </Grid>
-                <Grid item>
-                    <MenuOptions id={id} />
-                </Grid>
-            </Grid>
-            <Grid item container xs={12} alignItems='center' sx={{ marginTop: '10px' }}>
-                <Avatar />
-                <Typography className={classes.typography}>
-                    {assignedTo}
-                </Typography>
-            </Grid>
-        </Container>
+            </Container>
+            <Divider />
+        </>
     )
 }
 
